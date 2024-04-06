9aaf7c968648f7845e65bd99ba7a1a15093fdc85
sunny1561 <106393457+sunny1561@users.noreply.github.com>
2024-04-06 16:26:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Optional,Any
 from bson import ObjectId
 from fastapi import FastAPI, HTTPException, Path, Query, Body
 from pydantic import BaseModel
@@ -9,46 +9,48 @@ app = FastAPI()
 # Connect to MongoDB remotely
 client = MongoClient("mongodb+srv://sunnykumar:sunny450@cluster0.nggkwbc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client["Cosmocloud"]
-student_collection = db["student"]   
+student_collection = db["student"]
 
-# Define student model
-class Student(BaseModel):
-    name: str
-    age: int
-    address: Dict[str, str]
-#just to see     
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the Library Management System by Sunny Kumar!"}  
+    return {"message2": "Welcome to the Library Management System by Sunny Kumar!"}  
   
 
-# Create student 
+class Address(BaseModel):
+    city: str
+    country: str
+
+class Student(BaseModel):
+    name: Optional[str]  # Make the name field optional
+    age: Optional[int]
+    address: Optional[Address]
+
+# Create student
 @app.post("/students", response_model=Dict[str, str])
 async def create_student(student: Student):
     student_dict = student.dict()
     result = student_collection.insert_one(student_dict)
     return {"id": str(result.inserted_id)}
 
-
-
-# List  of students
+# List of students
 @app.get("/students", response_model=List[Student])
-async def list_students(country: str = Query(None), age: int = Query(None)):
+async def list_students(country: str = Query(None), age: int = None):
     query = {}
-    query = {}
-
     if country:
         query["address.country"] = country
-
-
     if age:
         query["age"] = {"$gte": age}
-
     students = list(student_collection.find(query))
-
     return students
 
 
+@app.patch("/students/{id}", response_model=Dict[str, str])
+async def update_student(id: str = Path(...), student_update: Dict[str, Any] = Body(...)):
+    result = student_collection.update_one({"_id": ObjectId(id)}, {"$set": student_update})
+    if result.modified_count == 1:
+        return {"message": "Student updated successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="Student not found")   
 
 # Fetch student with id
 @app.get("/students/{id}", response_model=Student)
@@ -58,23 +60,6 @@ async def fetch_student(id: str = Path(...)):
         return student
     else:
         raise HTTPException(status_code=404, detail="Student not found")
-    
-
-
-
-# Update student given id
-@app.patch("/students/{id}", response_model=Dict[str, str])
-async def update_student(id: str = Path(...), student: Student = Body(None)):
-    student_dict = student.dict()
-    result = student_collection.update_one({"_id": ObjectId(id)}, {"$set": student_dict})
-    if result.modified_count == 1:
-        return {"message": "Student updated successfully"}
-    else:
-        raise HTTPException(status_code=404, detail="Student not found")
-    
-
-
-
 
 # Delete student given id
 @app.delete("/students/{id}", response_model=Dict[str, str])
@@ -85,27 +70,25 @@ async def delete_student(id: str = Path(...)):
     else:
         raise HTTPException(status_code=404, detail="Student not found")
 
-
-
-# doc_dummy=[
+#doc_dummy=[
 
 #     {
-#         "name": "ansh",
+#         "name": "sde",
 #         "age": 22,
 #         "address": {"city": "London", "country": "UK"}
 #     },
 #     {
-#         "name": "manjeet",
+#         "name": "datasientist",
 #         "age": 25,
 #         "address": {"city": "Paris", "country": "France"}
 #     },
+    # {
+    #     "name": "MLengineer",
+    #     "age": 21,
+    #     "address": {"city": "Berlin", "country": "Germany"}
+    # },
 #     {
-#         "name": "Emily",
-#         "age": 21,
-#         "address": {"city": "Berlin", "country": "Germany"}
-#     },
-#     {
-#         "name": "saurabh",
+#         "name": "fullstack",
 #         "age": 23,
 #         "address": {"city": "Tokyo", "country": "Japan"}
 #     }
@@ -115,6 +98,5 @@ async def delete_student(id: str = Path(...)):
 #     student_collection.insert_one(json)
 
 
-#to run fastapipip 
+#to run fastapi application use below command
 # uvicorn main:app --reload
-
