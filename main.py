fe8a7ca58c8240f00c87aac73cf6cee61feb3af5
sunny1561 <106393457+sunny1561@users.noreply.github.com>
2024-04-24 19:01:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -117,8 +117,8 @@ db = client["Cosmocloud"]
 student_collection = db["student"]
 
 # Initialize Redis client
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
-
+# redis_client = redis.Redis(host='localhost', port=6379, db=0)
+redis_client = redis.from_url("redis://red-cokgfmud3nmc739b7m10:6379")
 # Maximum requests per day
 MAX_REQUESTS_PER_DAY = 500
 
